9246bfb35ef748f86bbfcff2b983b155c464e93d
Hakizimana Fred <hakifred20@gmail.com>
2023-09-15 20:11:43 +0300

    argv processes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { spawn } = require("child_process");
 
-const writerProcess = spawn("node", ["writer.js"]);
+const writerProcess = spawn("node", ["writer.js", ...process.argv.slice(2)]);
 const readerProcess = spawn("node", ["reader.js"]);
 
 // Pipe the writer process stdout to the main process (your terminal)
--- writer.js
+++ writer.js
@@ -1,1 +1,7 @@
-console.log("Hello, reader process!");
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("Hello there, simple reader");
+} else {
+  console.log("Arguments received :", args[0]);
+}
